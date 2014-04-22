f37823b337c5e4abd69d161186a98f1942a9db87
Zmago Devetak <zmago_devetak@yahoo.com>
2014-04-22 23:18:30 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,5 @@
 ## Put comments here that give an overall description of what your
-## functions do
+## functions do. The function take a matrix invert the numbers and return a new matrix
 
 ## Write a short comment describing this function
 ## this is a coment
